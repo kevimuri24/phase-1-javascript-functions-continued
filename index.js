ec3987e70d026bf63e9cc149b6bb8e8f7783e9f3
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2024-07-02 15:43:32 +0300

    Finished the Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(kitu = "roller-skate"){
+    return `This Saturday, I want to ${kitu}!`
+}
+saturdayFun("watch a movie")
+
+function mondayWork(kitu = "go to the office"){
+    return `This Monday, I will ${kitu}.`
+}
+mondayWork("finish that report")
+
+function wrapAdjective(noma){
+    let prompt = function (something) {
+        return `You are ${noma}${something}${noma}!`
+    }
+    return prompt;
+}
+console.log(wrapAdjective("*")("a hard worker"))
